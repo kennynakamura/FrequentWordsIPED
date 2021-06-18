088c51814254b31dc5db981bc755a51f2bf6059d
Kenny <kennynakamura@hotmail.com>
2021-06-18 09:17:20 -0300

    Update FrequentWordsTask.py

--- FrequentWordsTask.py
+++ FrequentWordsTask.py
@@ -1,13 +1,11 @@
 import os, re
 import heapq
+from java.lang import System
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
-
-configFile  = 'FrequentWordsConfig.txt'
-STOPProp  = 'STOP_det'
 
 class FrequentWordsTask:
     
@@ -18,51 +16,43 @@ class FrequentWordsTask:
         return True
         
     def init(self, confProps, configFolder):
-        
-        from java.io import File
-        from dpf.sp.gpinf.indexer.util import UTF8Properties
-        extraProps = UTF8Properties()
-        extraProps.load(File(configFolder, configFile))
-        STOP_det = extraProps.getProperty(STOPProp)      
-        global STOP
-        
-        def Convert(string):
-           li = list(string.split(" "))
-           return li
-           
-        if STOP_det is not None:
-           STOP = Convert(str(STOP_det)) 
-        return 
+        return
     
     def finish(self):      
         return 
         
     def process(self, item):
         
+        #Just Chats and Email
         categories = item.getCategorySet().toString()
         if not ("Chats" in categories or "Emails" in categories):
            return 
         
         text = str(item.getParsedTextCache()).lower()
-        #Retirando números
+        #Remove numbers
         text = re.sub(r'\d+','',text)
-        #Tokenização
+        #Tokenization
         tokenizer = RegexpTokenizer(r'\w+')
         Text_separed_from_words = tokenizer.tokenize(text)   
-        #Importando as stopwords do arquivo txt
-        All_stopwords = STOP
+
+        #Import stopwords from language settings in LocalConfig.txt 
+        language = System.getProperty('iped-locale')
+        if language == 'en':
+           All_stopwords = stopwords.words('en')
+        else:
+           All_stopwords = stopwords.words('portuguese')
         
+        #Function to remove stopwords from text
         def remove_stopwords(words, stopwords):
-           #Retirando as stop words
            words_without_stopwords = []
            for item in words:
               if item not in stopwords:
                  words_without_stopwords.append(item)
-           #Retirando palavras com menos de 3 letras
+           #Remove words with less then 3 letters
            for item in words_without_stopwords:
               if len(item) < 3:
                  words_without_stopwords.remove(item)
-           #Retirando risadas "kkk" 
+           #Remove laughter as "kkk" 
            for item in words_without_stopwords:
               characters_word = item.split()
               for i in characters_word:
@@ -70,7 +60,8 @@ class FrequentWordsTask:
                 if contLetra >= 2:
                    words_without_stopwords.remove(item)                 
            return words_without_stopwords
-         
+        
+        #Function to create Bag of Words
         def Create_bag_of_words(words):
            wordfreq ={}
            for item in words:
@@ -79,18 +70,25 @@ class FrequentWordsTask:
               else:
                  wordfreq[item] += 1     
            return wordfreq
-           
+        
+        #Removing imported stopwords from text
         words_without_stopwords = remove_stopwords(Text_separed_from_words, All_stopwords)
         
+        #Stemming function - Stemming is the process of reducing a word to its word stem that affixes to suffixes and prefixes or to the roots of words known as a lemma.
         def stemming(words):
             stem = PorterStemmer()
             for item in words:
                item = stem.stem(item)
             return words
-        #Stemmização das palavras -- Não tenho certeza se está fazendo muita diferença, mas é uma função do NLTK
+        
+        #Stemming words in text
         words_without_stopwords = stemming(words_without_stopwords)
         
+        #Creating Bag of Words from stemming words
         Bag_of_words = Create_bag_of_words(words_without_stopwords)
-        #Seleção das 10 palavras com maiores frequências
-        most_freq = heapq.nlargest(10, Bag_of_words, key=Bag_of_words.get) 
+        
+        #Selection of the top 10 words by frequency
+        most_freq = heapq.nlargest(10, Bag_of_words, key=Bag_of_words.get)
+        
+        #Setting most frequency words in Extra Attributes in FrequentWords
         item.setExtraAttribute('FrequentWords', most_freq)  
